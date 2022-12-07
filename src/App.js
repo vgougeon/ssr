a8b7928722020c2046706357e0425f5d9cb2e0cb
Vincent GOUGEON <gougeonvincent31@gmail.com>
2022-12-07 14:46:50 +0100

    feat: get groups

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/groups')
+      .then((response) => response.json())
+      .then((data) => setGroups(data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      ce texte est rendu en ssr
+      {groups.map(g =>
+        <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '30px' }}>
+          {g.map(student =>
+            <span>{student}</span>
+          )}
+        </div>
+      )}
     </div>
   );
 }
